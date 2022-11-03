7888760bebf44e25bc80a7e54c1c21f7dc3dd227
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-03 15:36:57 +0530

    check happy test case

--- MoodAnalyzerTest/MoodAnalyzerMain.java
+++ MoodAnalyzerTest/MoodAnalyzerMain.java
@@ -4,10 +4,10 @@ public class MoodAnalyzerMain {
 	
 	//UC1 Given a Message, ability to analyse and respond  Happy or Sad Mood
 	
-	//TC1Given “I am in Sad Mood” messag Should Return SAD
+	//TC1.2 Given “I am in Any Mood” messageShould Return HAPPY
 
 	public String analyseMood(String message) {
-		return "SAD";
+		return "HAPPY";
 
 
 	}
