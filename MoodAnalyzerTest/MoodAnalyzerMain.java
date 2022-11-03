700050e1aaef5fba2834378aea27695972dc0bb7
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-03 15:14:35 +0530

    start junit test case

---
+++ MoodAnalyzerTest/MoodAnalyzerMain.java
@@ -0,0 +1,12 @@
+package com.moodanalyzer.MoodAnalyzerTest;
+
+public class MoodAnalyzerMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Mood Analyzer JUnit Test Case");
+	}
+		
+	}
+	
+
---
+++ MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -0,0 +1,17 @@
+package com.moodanalyzer.MoodAnalyzerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class MoodAnalyzerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	}
+
