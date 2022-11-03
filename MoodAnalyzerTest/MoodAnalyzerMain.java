aee06ac8450a103051e1f25537d3f74cfb56f2c9
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-03 15:31:58 +0530

    check sad mood test case

--- MoodAnalyzerTest/MoodAnalyzerMain.java
+++ MoodAnalyzerTest/MoodAnalyzerMain.java
@@ -1,8 +1,13 @@
 package com.moodanalyzer.MoodAnalyzerTest;
 
 public class MoodAnalyzerMain {
+	
+	//UC1 Given a Message, ability to analyse and respond  Happy or Sad Mood
+	
+	//TC1Given “I am in Sad Mood” messag Should Return SAD
+
 	public String analyseMood(String message) {
-		return null;
+		return "SAD";
 
 
 	}
