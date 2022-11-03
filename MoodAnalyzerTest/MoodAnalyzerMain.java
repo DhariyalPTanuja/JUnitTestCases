32278dd5df30ecdfcced61e6b8925337bc80631a
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-04 00:23:23 +0530

    take the mood message in constructor test case

--- MoodAnalyzerTest/MoodAnalyzerMain.java
+++ MoodAnalyzerTest/MoodAnalyzerMain.java
@@ -1,11 +1,29 @@
 package com.moodanalyzer.MoodAnalyzerTest;
 
 public class MoodAnalyzerMain {
-	public String analyseMood(String message) {
-		return null;
-
+	//Refactor the code to take the mood message in Constructor
+	private String message;
+	 MoodAnalyzerMain() {
+		message="SAD";
+	}
+ MoodAnalyzerMain(String message) {
+	this.message = message;
 
 	}
+	
+	public String getMessage() {
+	return message;
+}
+
+		 
+	
+	
+	
+	
+
+	
 	}
+	
+	
 	
 
--- MoodAnalyzerTest/MoodAnalyzerTest.java
+++ MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -7,19 +7,18 @@ import org.junit.jupiter.api.Test;
 import junit.framework.Assert;
 
 class MoodAnalyzerTest {
+	
 //UC1 Given a Message, ability to analyse and respond  Happy or Sad Mood
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() {
 		MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
-		String mood = moodAnalyser.analyseMood("I am in SAD Mood");
-
+		String mood = moodAnalyser.getMessage();
 		Assert.assertEquals("SAD", mood);
 	}
-
 	@Test
 	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-		MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
-		String mood = moodAnalyser.analyseMood("I am in any Mood");
+		MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("HAPPY");
+		String mood = moodAnalyser.getMessage();
 
 		Assert.assertEquals("HAPPY", mood);
 	}
